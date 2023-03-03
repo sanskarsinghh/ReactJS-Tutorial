c71eddd0309181c3d0baa32f37513c48b4329475
sanskarsingh <sanskarsingh.college@gmail.com>
2023-03-03 17:24:32 +0530

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,18 @@ import About from './About';
 import './App.css';
 import Navbar from './Navbar';
 // import Textform from './Textform';
+import React, {useState} from 'react';
+
+
 
 
 
 function App() {
+  const [mode, setmode] = useState('dark');
   return (
     <>
 
-      <Navbar title="Text-utils" />
+      <Navbar title="Text-utils" mode = {mode}/>
       <div className="container my-3">
       {/* <Textform  heading="Enter the text to analyze "/> */}
       <About/>
