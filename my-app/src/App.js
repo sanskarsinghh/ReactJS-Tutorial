c6fa215947cae5129c63216a201c6e4be3e61664
sanskarsingh <sanskarsingh.college@gmail.com>
2023-03-04 18:17:11 +0530

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,32 @@
 // import logo from './logo.svg';
-import About from './About';
+// import About from './About';
 import './App.css';
 import Navbar from './Navbar';
-// import Textform from './Textform';
-import React, {useState} from 'react';
+import Textform from './Textform';
+import Alert from './Alert';
+import { useState } from 'react';
 
 
 
 
 
 function App() {
-  const [mode, setmode] = useState('dark');
+  const [alert, setAlert] = useState("null");
+  const showAlert = (message, type) => {
+    setAlert({
+      msg : message,
+      type : type
+    })
+
+  }
   return (
     <>
 
-      <Navbar title="Text-utils" mode = {mode}/>
+      <Navbar title="Text-utils" />
+        <Alert alert={alert}/>
       <div className="container my-3">
-      {/* <Textform  heading="Enter the text to analyze "/> */}
-      <About/>
+        <Textform heading="Enter the text to analyze " />
+        {/* <About/> */}
       </div>
 
 
